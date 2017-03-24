79ea3872282f07407477c8be486da282baa4a6b4
liushuwei <liushuwei@qiyi.com>
2017-03-24 14:58:44 +0800

    增加数据存储的多种用法

--- app/src/main/java/com/lsw/demo/MainActivity.java
+++ app/src/main/java/com/lsw/demo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lsw.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button fileButton = (Button)findViewById(R.id.file_storage);
+        fileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,FileStorageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button SPButton = (Button)findViewById(R.id.sp_button);
+        SPButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,SharedPreferencesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button dataBaseButton = (Button)findViewById(R.id.database_button);
+        dataBaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,DataBaseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/lsw/demo/SharedPreferencesActivity.java
@@ -0,0 +1,49 @@
+package com.lsw.demo;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SharedPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preferences);
+        Button saveData = (Button) findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+        Button restoreData = (Button) findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("SpActivity", "name is " + name);
+                Log.d("SpActivity", "age is " + age);
+                Log.d("SpActivity", "married is " + married);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+        editor.clear();
+        editor.apply();
+    }
+}
